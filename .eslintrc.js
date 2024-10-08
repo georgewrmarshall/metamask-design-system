89910316cebab62f48f417f7bc1299a2c48ab81a
George Marshall <georgewrmarshall@gmail.com>
2024-10-08 14:47:20 -0700

    Migrate from polyrepo to monorepo (#17)
    ## **Description**
    
    This PR migrates the current MetaMask design system repo which is
    currently using the metamask-module-template and a polyrepo structure to
    a monorepo structure. The primary goal is to streamline the module
    management process by consolidating multiple repositories into a single
    monorepo. The `core` module serves as a reference for the files and
    structure required in the monorepo setup. This change will simplify
    dependency management and improve development efficiency across modules.
    
    <img width="444" alt="Screenshot 2024-10-08 at 2 09 12 PM"
    src="https://github.com/user-attachments/assets/eb1dcb2e-4eb1-45bb-833f-4fbe3295c84f">
    
    ### Key changes:
    - Updated module configuration to align with the monorepo structure.
    - Consolidated files and dependencies into the monorepo format.
    - Ensured consistency with the `core` module in terms of file inclusion,
    structure, and build processes.
    
    ## **Related issues**
    
    Fixes: https://github.com/MetaMask/metamask-design-system/issues/5
    
    ## **Manual testing steps**
    
    1. Clone the repository and navigate to the monorepo.
    2. Run all `yarn` scripts to ensure they work with the exception of
    `prepare-preview-build` and `publish-previews`
    3. Verify that the scriptsfunction as expected within the monorepo.
    4. Ensure the build passes without issues in the monorepo format.
    5. Ensure no instances of "core" remain unless links to information for
    migrating packages
    
    ## **Screenshots/Recordings**
    
    Video showing all `yarn` scripts work from `package.json` with the
    exception of `prepare-preview-build` and `publish-previews` issue here:
    https://github.com/MetaMask/metamask-design-system/issues/26
    
    https://www.loom.com/share/4f40fa33ae45472e807137d3995c212c
    
    ## **Pre-merge author checklist**
    
    - [x] I’ve followed [MetaMask Contributor
    Docs](https://github.com/MetaMask/contributor-docs)
    - [x] I’ve completed the PR template to the best of my ability
    - [x] I’ve included tests if applicable
    - [x] I’ve documented my code using [JSDoc](https://jsdoc.app/) format
    if applicable
    - [x] I’ve applied the right labels on the PR (see [labeling
    guidelines](https://github.com/MetaMask/metamask-extension/blob/develop/.github/guidelines/LABELING_GUIDELINES.md)).
    
    ## **Pre-merge reviewer checklist**
    
    - [ ] I've manually tested the PR (e.g. pull and build branch, run the
    app, test code being changed).
    - [ ] I confirm that this PR addresses all acceptance criteria described
    in the ticket it closes and includes the necessary testing evidence such
    as recordings and/or screenshots.

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,50 +1,76 @@
 module.exports = {
   root: true,
-
-  extends: ['@metamask/eslint-config'],
-
+  extends: ['@metamask/eslint-config', '@metamask/eslint-config-nodejs'],
+  ignorePatterns: [
+    '!.eslintrc.js',
+    '!jest.config.js',
+    'node_modules',
+    '**/dist',
+    '**/docs',
+    '**/coverage',
+    'merged-packages',
+    'scripts/create-package/package-template',
+  ],
   overrides: [
+    {
+      files: ['*.test.{ts,js}'],
+      extends: ['@metamask/eslint-config-jest'],
+    },
+    {
+      files: ['*.js', '*.cjs'],
+      parserOptions: {
+        sourceType: 'script',
+        ecmaVersion: '2020',
+      },
+    },
     {
       files: ['*.ts'],
       extends: ['@metamask/eslint-config-typescript'],
-    },
-
-    {
-      files: ['*.js'],
       parserOptions: {
-        sourceType: 'script',
+        tsconfigRootDir: __dirname,
+        project: ['./tsconfig.packages.json'],
       },
-      extends: ['@metamask/eslint-config-nodejs'],
+      rules: {
+        '@typescript-eslint/no-explicit-any': 'error',
+      },
     },
-
     {
-      files: ['yarn.config.cjs'],
-      parserOptions: {
-        sourceType: 'script',
-        ecmaVersion: 2020,
+      files: ['*.d.ts'],
+      rules: {
+        '@typescript-eslint/naming-convention': 'warn',
+        'import/unambiguous': 'off',
       },
-      settings: {
-        jsdoc: {
-          mode: 'typescript',
-        },
-      },
-      extends: ['@metamask/eslint-config-nodejs'],
     },
-
     {
-      files: ['*.test.ts', '*.test.js'],
-      extends: [
-        '@metamask/eslint-config-jest',
-        '@metamask/eslint-config-nodejs',
-      ],
+      files: ['scripts/**/*.ts'],
+      rules: {
+        'import/no-unassigned-import': 'off',
+        'n/no-sync': 'off',
+        // All scripts will have shebangs.
+        'n/shebang': 'off',
+      },
     },
   ],
-
-  ignorePatterns: [
-    '!.eslintrc.js',
-    '!.prettierrc.js',
-    'dist/',
-    'docs/',
-    '.yarn/',
-  ],
+  rules: {
+    camelcase: 'off',
+    'id-length': 'off',
+    'no-restricted-syntax': [
+      'error',
+      'ForInStatement',
+      'LabeledStatement',
+      'WithStatement',
+    ],
+    'jsdoc/match-description': [
+      'off',
+      { matchDescription: '^[A-Z`\\d_][\\s\\S]*[.?!`>)}]$' },
+    ],
+  },
+  settings: {
+    'import/resolver': {
+      typescript: {},
+    },
+    jsdoc: {
+      mode: 'typescript',
+    },
+  },
 };
