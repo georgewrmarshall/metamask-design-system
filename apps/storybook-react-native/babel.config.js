96ba91d7f72ecae95e103b7a940f5d732dd4f5ae
Brian August Nguyen <brianacnguyen@gmail.com>
2024-11-04 13:34:26 -0800

    Reverting React-Native framework-less PR (#80)
    <!--
    Please submit this PR as a draft initially.
    Do not mark it as "Ready for review" until the template has been
    completely filled out, and PR status checks have passed at least once.
    -->
    
    ## **Description**
    This PR reverts the React-Native framework-less environment, in
    anticipation for implementation Expo React Native
    <!--
    Write a short description of the changes included in this pull request,
    also include relevant motivation and context. Have in mind the following
    questions:
    1. What is the reason for the change?
    2. What is the improvement/solution?
    -->
    
    ## **Related issues**
    
    Fixes: N/A
    
    ## **Manual testing steps**
    
    1. Go to this page...
    2.
    3.
    
    ## **Screenshots/Recordings**
    
    <!-- If applicable, add screenshots and/or recordings to visualize the
    before and after of your change. -->
    
    ### **Before**
    
    <!-- [screenshots/recordings] -->
    
    ### **After**
    
    <!-- [screenshots/recordings] -->
    
    ## **Pre-merge author checklist**
    
    - [x] I've followed [MetaMask Contributor
    Docs](https://github.com/MetaMask/contributor-docs)
    - [x] I've completed the PR template to the best of my ability
    - [ ] I’ve included tests if applicable
    - [ ] I’ve documented my code using [JSDoc](https://jsdoc.app/) format
    if applicable
    - [x] I’ve applied the right labels on the PR (see [labeling
    guidelines](https://github.com/MetaMask/metamask-extension/blob/develop/.github/guidelines/LABELING_GUIDELINES.md)).
    Not required for external contributors.
    
    ## **Pre-merge reviewer checklist**
    
    - [ ] I've manually tested the PR (e.g. pull and build branch, run the
    app, test code being changed).
    - [ ] I confirm that this PR addresses all acceptance criteria described
    in the ticket it closes and includes the necessary testing evidence such
    as recordings and or screenshots.
    
    ---------
    
    Co-authored-by: georgewrmarshall <george.marshall@consensys.net>

--- apps/storybook-react-native/babel.config.js
+++
@@ -1,9 +0,0 @@
-/* eslint-disable import/unambiguous */
-module.exports = {
-  presets: ['module:metro-react-native-babel-preset'],
-  plugins: [
-    ['@babel/plugin-transform-class-properties', { loose: false }],
-    ['@babel/plugin-transform-private-methods', { loose: false }],
-    ['@babel/plugin-transform-private-property-in-object', { loose: false }],
-  ],
-};
--- apps/storybook-react-native/react-native.config.js
+++
@@ -1,8 +0,0 @@
-/* eslint-disable import/unambiguous */
-module.exports = {
-  project: {
-    ios: {},
-    android: {},
-  },
-  dependencies: {},
-};
